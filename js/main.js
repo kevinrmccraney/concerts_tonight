329250f57dc8c72521a79734630388841fff9459
Kevin R. McCraney <kevinrmccraney@gmail.com>
2015-04-17 11:38:12 -0400

    cleanup and add readme.md

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-// get the current date as an object for comparison
-var currentDate = new Date();
-var dd = currentDate.getDate()+1;
-var mm = currentDate.getMonth()+1;
-var yyyy = currentDate.getFullYear();
-
-if(dd<10) {
-    dd='0'+dd};
-
-if(mm<10) {
-    mm='0'+mm};
-
-today = yyyy+'-'+mm+'-'+dd;
-
 // create a map in the map div
 var map = L.map('map');
 
@@ -33,15 +19,13 @@ function onLocationFound(e) {
 }
 map.on('locationfound', onLocationFound);
 
-
-// if they deny geolocation, map center defaults to the Newport
+// if they deny geolocation, map center defaults to the Newport, in Columbus
 function onLocationError(e) {
     alert(e.message);
 }
 map.on('locationerror', onLocationError).setView([39.9975, -83.0074], 13);
 
-
-// Using the core $.ajax() method
+// Using getJSON to get data, and create markers
 var promise = $.getJSON( 'http://api.seatgeek.com/2/events?geoip=true');
 promise.then(function(data){
     var events = data.events;
@@ -49,41 +33,11 @@ promise.then(function(data){
     $.each(events, function(i){
 
             var marker = L.marker( [events[i]['venue']['location']['lat'], events[i]['venue']['location']['lon']]).addTo(map)
-            .bindPopup('<h5>' +events[i]['title']+ ' at '+events[i]['venue']['name'] + '</h5>  starting at ' + events[i]['datetime_local'].slice(11, 16) + ". Don't be late!")
+            .bindPopup('<b>' +events[i]['title']+ '</b> is at <b>'+
+                events[i]['venue']['name'] + '</b>  starting at <b>' + events[i]['datetime_local'].slice(11, 16) + "</b>. Don't be late!")
             .addTo(map)
             .on('mouseover', function(){
                 this.openPopup();
             });
-
     });
-
 });
-
-//jquery button selection, not yet implemented
-$("#today").click(function(){
-});
-
-$("#tomorrow").click(function(){
-    map.addLayer(tomorrow);
-    map.removeLayer(today);
-    map.removeLayer(later);
-});
-
-$("#later").click(function(){
-    map.addLayer(later);
-    map.removeLayer(tomorrow);
-    map.removeLayer(today);
-});
-
-$("#all").click(function(){
-    map.addLayer(today);
-    map.addLayer(tomorrow);
-    map.addLayer(later);
-});
-
-
-
-// create markers
-//L.marker([39.9975, -83.0024], {icon:iconBasic}).addTo(map)
-//    .bindPopup('<p><h3>The {artist} Suftan Stevens</h3>{time} at {place}</p>')
-//    .openPopup();
